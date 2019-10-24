aeec45479aed06376926736b1d2261cd1cab4a4f
Brandon Toy <bgh.toy@gmail.com>
2019-10-24 17:47:04 -0500

    Added dj khaled random quote

--- README.md
+++ README.md
@@ -13,5 +13,8 @@ Commands:
     - When you can't take anymore of Khaled's shit
 - `dj khaled play im the one`
     - Im the One ft Chance the Rapper and other people
+- `dj khaled random quote`
+    - Says a random quote from the one and only
+    - Couldn't find a working dj khaled quote generator api :(
 
 More Commands in the works!
--- index.js
+++ index.js
@@ -5,6 +5,48 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 let botChannelID = "";
+let djKhaledQuotes = [
+    "Another one, no. Another two, drop two singles at a time.",
+    "Congratulations, you played yourself.",
+    "I can deal with everything. I got the answer for anything. This DJ Khaled.",
+    "The key is to make it.",
+    "There will be road blocks but we will overcome it.",
+    "They will try to close the door on you, just open it.",
+    "The key to more success is coco butter.",
+    "They dont want you to jet ski.",
+    "Those that weather the storm are the great ones.",
+    "You smart! You loyal! Youre a genius!",
+    "Baby, you smart. I want you to film me taking a shower.",
+    "You want my advice? Dont play yourself.",
+    "Always have faith. Always have hope.",
+    "Key to more success is clean heart and clean face.",
+    "Smh they get mad when u have joy.",
+    "I dont have no favorite rock bands. Im a fan of rock music though.",
+    "I wanted to see what type of trees is growing in Marcy Projects, what type of water Jay Z was drinking.",
+    "Those that weather the storm r the great ones.",
+    "The other day the grass was brown, its green cuz i aint give up.",
+    "Never surrender.",
+    "Give thanks to the most high.",
+    "They dont want you to win. They dont want you to have the No. record in the country. They dont want you get healthy. They dont want you to exercise. And they dont want you to have that view.",
+    "We go hard. In everything we do were going to accomplish our victory and our goal. If it takes a day, a year, or 20 years, were going to win.",
+    "Bless up.",
+    "Always have Faith. Always have Hope",
+    "I know that Ive been put on this Earth to make people happy, to inspire people, and to uplift people. Thats a beautiful thing.",
+    "Be a star. Be a Superstar.",
+    "I remember when I aint have a jacuzzi.",
+    "Almond milk + cinnamon crunch = major key to success.",
+    "When you stop making excuses and you work hard and go hard you will be very successful.",
+    "The key is to enjoy life, because they dont want you to enjoy life.",
+    "In life everyone has a choice. The key is: make a right choice.",
+    "Bless up. Dont play yourself.",
+    "You do know it cost money to put a t-shirt on your back? You do know it cost money have a house? You do know it cost money to eat? Get money, dont let these people fool you.",
+    "We have to get money. We have no choice. It cost money to eat.",
+    "They never said winning was easy.",
+    "Working all winter shining all summer.",
+    "Give thanks to the most high.",
+    "Some of the guys when they play, they try to keep it reality. Nah, I need the best everything.",
+    "The key is: never fold."
+]
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
@@ -49,6 +91,15 @@ client.on('message', async msg => {
     }
 })
 
+client.on('message', async msg => {
+    if(msg.content === 'dj khaled random quote') {
+        const length = djKhaledQuotes.length
+        const randomIndex = Math.floor(Math.random() * length)
+        msg.reply(djKhaledQuotes[randomIndex])
+    }
+})
+
+// play music
 client.on('message', async msg => {
     if(msg.content === 'dj khaled play im the one') {
         voiceChannelID = msg.member.voice.channelID;
